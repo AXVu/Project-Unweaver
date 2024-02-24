3a1d56caf279625af95b4fbb28706885c6743b03
clarkc0de <Dzawacki@clarku.edu>
2024-02-24 11:34:04 -0500

    site can take pictures

--- unravel/myapp/views.py
+++ unravel/myapp/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import PictureForm
+
+def upload_picture(request):
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirect to homepage after successful upload
+    else:
+        form = PictureForm()
+    return render(request, 'upload_picture.html', {'form': form})
 
 def index(request):
     return HttpResponse("Hello, world!")
--- unravel/unravel/settings.py
+++ unravel/unravel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
+    'unravel',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media settings for uploading pictures
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
