27c5912a8cfebb28f3e5ae1354800df982ccf536
Alexander Vu <avu@clarku.edu>
2024-02-24 09:15:57 -0500

    add barcode reader
    barcode_reader only has 3 functions,
    file_to_barcode, which takes an input file
    and returns the barcode in the image
    barcode_to_brand, which takes an input barcode string and returns the brand
    file_to_brand, which performs both functions

---
+++ barcode_reader.py
@@ -0,0 +1,32 @@
+import pyzxing as pyx
+import requests
+import json
+
+def file_to_barcode(file):
+    read = pyx.BarCodeReader()
+    result = read.decode(file)
+    if "parsed" in result[0].keys():
+        encoded = result[0]["parsed"]
+        barcode = encoded.decode('utf-8')
+        print(f"Got barcode {barcode}")
+        return barcode
+    else:
+        return "Failed to read barcode"
+
+def barcode_to_brand(barcode):
+    if barcode == "Failed to read barcode":
+        return barcode
+    headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Accept-Encoding': 'gzip,deflate',
+    }
+
+    resp = requests.get(f'https://api.upcitemdb.com/prod/trial/lookup?upc={barcode}', headers=headers)
+    data = json.loads(resp.text)
+    if data['total'] == 0:
+        return "Barcode read; Failed to find product"
+    return data['items'][0]['brand']
+
+def file_to_brand(file):
+    return barcode_to_brand(file_to_barcode(file))
