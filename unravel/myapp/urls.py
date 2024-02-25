a931ec5ff941f1723199a71021b9c9732ec1d6ed
clarkc0de <Dzawacki@clarku.edu>
2024-02-25 11:46:02 -0500

    this works

--- unravel/myapp/urls.py
+++ unravel/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import upload_picture, delete_image
 
 urlpatterns = [
-    path('', views.upload_picture, name='upload')
+    path('', views.upload_picture, name='upload'),
+    path('delete/', delete_image, name='delete_image'),
 ]
--- unravel/myapp/views.py
+++ unravel/myapp/views.py
@@ -4,18 +4,25 @@ from .forms import PictureForm
 from myapp.barcode_reader import file_to_brand
 import pandas as pd
 from django.apps import apps
+from django.conf import settings
+import os
 
 def upload_picture(request):
+    brand = ""
     if request.method == 'POST':
         form = PictureForm(request.POST, request.FILES)
+        
         if form.is_valid():
             picture = form.save()
-            pic_path = picture.image.path
-            brand = file_to_brand(pic_path)
-            return HttpResponse(f"Brand {brand} is {brand_check(brand)}")  # Redirect to homepage after successful upload
+            request.session['image_url'] = picture.image.url
+            brand = file_to_brand(picture.image.path)
+            return render(request, 'upload_picture.html', {'form': form, 'image_url': picture.image.url, "brand": brand, "sus": brand_check(brand)})
     else:
         form = PictureForm()
-    return render(request, 'upload_picture.html', {'form': form})
+    
+    image_url = request.session.get('image_url', None)
+
+    return render(request, 'upload_picture.html', {'form': form, 'image_url': image_url, "brand": brand, "sus": brand_check(brand)})
 
 def index(request):
     return HttpResponse("Hello, world!")    
@@ -28,3 +35,13 @@ def brand_check(brand):
         return df.to_numpy()[0][1]
     else:
         return f"{brand} not in database"
+def delete_image(request):
+    
+    if request.method=='POST':
+
+        if 'image_url' in request.session:
+            image_path = os.path.join(settings.MEDIA_ROOT, request.session['image_url'])
+            if os.path.exists(image_path):
+                os.remove(image_path)
+            del request.session['image_url']
+    return redirect('upload_picture')
